c56e04876f700f5fd28220c4573018aa3b0f9b25
RayKoltner <andrejkoltner@gmail.com>
2024-11-28 20:57:16 +0300

    Added prototype for universal mapper

---
+++ NSIDictionaryService.Data/Models/DictProperty.cs
@@ -0,0 +1,13 @@
+﻿namespace NSIDictionaryService.Data.Models
+{
+    public class DictProperty: BaseEntity
+    {
+        public string PropertyName { get; set; } = String.Empty; //Name of the property in dict class
+
+        public string PropertyCode {  get; set; } = String.Empty; //Name of the property in XML
+
+        public string PropertyType { get; set; } = String.Empty;
+
+        public string PropertyMethod {  get; set; } = String.Empty; //"" for string type
+    }
+}
