fd758efc35d203bb0f31e94e5d4dde51636b3dc2
RayKoltner <andrejkoltner@gmail.com>
2024-11-28 21:35:37 +0300

    A little tweak to DictProperty

--- NSIDictionaryService.Data/Models/DictProperty.cs
+++ NSIDictionaryService.Data/Models/DictProperty.cs
@@ -2,6 +2,7 @@
 {
     public class DictProperty: BaseEntity
     {
+        public string DictionaryName { get; set; } = String.Empty; //Name of the dictionary itself
         public string PropertyName { get; set; } = String.Empty; //Name of the property in dict class
 
         public string PropertyCode {  get; set; } = String.Empty; //Name of the property in XML
